40c9825e3467659dbf3e8b619362651ad9e6bce4
ridhoafriyando <78814296+ridhoafriyando@users.noreply.github.com>
2021-06-24 15:29:38 +0700

    UAS PBO 2021 KELAS A

---
+++ TugasAkhirController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+class TugasAkhirController
+{
+    public function daftarkanTA()
+    {
+       
+    }
+
+    public function ubahTA()
+    {
+        
+    }
+
+    public function validasiPendaftaran()
+    {
+       
+    }
+
+    public function inputNilaiSempro()
+    {
+        
+    }
+
+    public function inputNilaiSemhas()
+    {
+      
+    }
+
+    public function inputNilaiSidang()
+    {
+      
+    }
+} 
